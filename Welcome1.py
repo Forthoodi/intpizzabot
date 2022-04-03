a6c76a2ab47453a1a07cbb46425e3c21a414df8d
Forthoodi <99222935+Forthoodi@users.noreply.github.com>
2022-04-03 18:37:23 +1200

    Create Welcome1.py

---
+++ Welcome1.py
@@ -0,0 +1,15 @@
+import random
+from random import randint
+
+names = [ "Mikara", "Louis" , "Jodek " , "Karlo " ,"Tomas", "James" , "Marcus" , "Jan" , "Tomas" ]
+
+num = randint(0,9)
+name = (names[num])
+print ("*** Welcome to Shoes Shop  ***") 
+print("***My name is",name,"***")
+print("Here to help please order ")
+
+
+
+
+

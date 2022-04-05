2c5ac3458bbe6ab64fe9542c888f2960b532c96d
Forthoodi <99222935+Forthoodi@users.noreply.github.com>
2022-04-05 12:14:04 +1200

    5/3/22

---
+++ ShoeShop1.py
@@ -0,0 +1,77 @@
+#Welcome message with random names
+import random
+from random import randint
+
+#list of names
+names = [ "Mikara", "Louis" , "Jodek " , "Karlo " ,"Tomas", "James" , "Marcus" , "Jan" , "Tomas" ]
+
+#Menu for In store or Delivery    
+def welcome():
+      """""
+    Purpose : To run all functions  
+    a Welcome message
+    Parameters : None 
+    Returns : None 
+    """
+num = randint(0,9)
+name = (names[num])
+print ("*** Welcome to Shoes Shop  ***") 
+print("***My name is",name,"***")
+print("Here to help please order ")
+
+
+#Pick up information = name and andress 
+
+
+
+
+
+#Choose total  number of shoes 
+
+
+
+
+
+#Shoe types
+
+
+
+
+#Shoe order - from menu - print each shoe order with cost 
+
+
+
+
+#Shoe order print out 
+
+
+
+
+
+
+#Abilly to cancel or accept
+
+
+
+
+#Option for new order or exit 
+
+
+
+
+
+#Main Function 
+def main():
+     """""
+    Purpose : To run all functions  
+    a Welcome message
+    Parameters : None 
+    Returns : None 
+     """
+welcome()
+    
+ 
+
+
+
+main()
--- welcome2.py
+++ welcome2.py
@@ -10,7 +10,34 @@ def welcome():
     name = (names[num])
     print ("*** Welcome to Shoes Shop  ***") 
     print("***My name is",name,"***")
-    print("Here to help please order ")
+    print("Here to help please order***")
+
+
+#list of names
+names = [ "Mikara", "Louis" , "Jodek " , "Karlo " ,"Tomas", "James" , "Marcus" , "Jan" , "Tomas" ]
+def welcome():
+    
+    num = randint(0,9)
+    name = (names[num])
+    print ("*** Welcome to Shoes Shop  ***") 
+    print("***My name is",name,"***")
+    print("Here to help please order ***")
+
+
+def main():
+         """""
+    Purpose : To run all functions  
+    a Welcome message
+    Parameters : None 
+    Returns : None 
+     """
+welcome()
+    
+ 
+
+
+
+main()
 
 
 def main():
